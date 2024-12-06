49e7b5bcefcb7a4ebb91c6cd9d93e63c04638457
nafistarik <nafistarik789@gmail.com>
2024-12-06 23:45:28 +0600

    some text issues fixed

--- app/components/testimonial.js
+++ app/components/testimonial.js
@@ -10,7 +10,7 @@ export default function Testimonial() {
       </div>
         <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-2">What Our Customers Say</h2>
         <p className="text-gray-600 mb-8 w-full md:w-2/3 mx-auto">
-            Don't just take our word for it - here's what some of our customers have to say about their experience with Fresh Harvest:
+        Don&apos;t just take our word for it - here&apos;s what some of our customers have to say about their experience with Fresh Harvest:
         </p>
         <div className="flex flex-col md:flex-row items-center justify-center">
             <div className="mb-6 md:mb-0 md:mr-6 basis-11/12">
@@ -18,7 +18,7 @@ export default function Testimonial() {
             </div>
             <div className="bg-gray-100 p-6 rounded-lg shadow-md">
                 <p className="text-gray-700 text-lg mb-4">
-                    "I absolutely love Fresh Harvest! The quality of their produce is outstanding. It's always fresh, flavorful, and delicious. The convenience of ordering online and having it delivered to my doorstep saves me so much time. Fresh Harvest has become my go-to for all my fruit and vegetable needs."
+                &quot;I absolutely love Fresh Harvest! The quality of their produce is outstanding. Its always fresh, flavorful, and delicious. The convenience of ordering online and having it delivered to my doorstep saves me so much time. Fresh Harvest has become my go-to for all my fruit and vegetable needs.&quot;
                 </p>
                 <p className="font-bold text-gray-900">
                     Jane Doe - 
